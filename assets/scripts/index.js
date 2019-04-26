a62e29744a97d94a343918bfcac644ab09bc6bb1
Phoenix <directioner0829@Phoenixs-MacBook-Air-2.local>
2019-04-26 11:38:37 -0700

    initial push

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,24 +1,43 @@
 
+	var red = "#D56969";
+	var blue = "#ADCAD3";
+	var grey = "#707070";
+
+
 	$("#red-circle").click(function() {
     var back = document.getElementById("page");
     var right_line = document.getElementById("red-line");
     var left_line = document.getElementById("blue-line");
     var nav_items = document.getElementsByTagName("a");
+    var stroke1 = document.getElementsByClassName("text text-stroke text-stroke-2");
+    var the_overlay_color = document.getElementById("overlay-color");
+    var the_overlay = document.getElementById("overlay");
+
 
 	if(back.style.background === "rgb(213, 105, 105)"){
-     	back.style.background = "white";
-        left_line.style.background = "#ADCAD3";
-        right_line.style.background = "#D56969";
         for (var i = 0; i < nav_items.length; i++) {
-        	nav_items[i].style.color = "#707070";
+        	nav_items[i].style.color = grey;
         }
+        for (var i = 0; i < stroke1.length; i++) {
+        	stroke1[i].style.stroke = blue;
+        }
+        back.style.background = "white";
+        left_line.style.background = blue;
+        right_line.style.background = red;
+        the_overlay_color.style.zIndex = 1;
+        the_overlay.style.zIndex = 3;
     }else{
-        back.style.background = "#D56969";
-        left_line.style.background = "#ADCAD3";
-        right_line.style.background = "#707070";
         for (var i = 0; i < nav_items.length; i++) {
-        	nav_items[i].style.color = "#707070";
+        	nav_items[i].style.color = grey;
         }
+        for (var i = 0; i < stroke1.length; i++) {
+        	stroke1[i].style.stroke = blue;
+        }
+        back.style.background = red;
+        left_line.style.background = blue;
+        right_line.style.background = grey;
+        the_overlay_color.style.zIndex = 3;
+        the_overlay.style.zIndex = 1;
     }
   });
 	$("#blue-circle").click(function() {
@@ -26,21 +45,36 @@
     var right_line = document.getElementById("red-line");
     var left_line = document.getElementById("blue-line");
     var nav_items = document.getElementsByTagName("a");
+    var stroke1 = document.getElementsByClassName("text text-stroke text-stroke-2");
+    var the_overlay_color = document.getElementById("overlay-color");
+    var the_overlay = document.getElementById("overlay");
+
 
 	if(back.style.background === "rgb(173, 202, 211)"){
+        for (var i = 0; i < nav_items.length; i++) {
+        	nav_items[i].style.color = grey;
+        }
+        for (var i = 0; i < stroke1.length; i++) {
+        	stroke1[i].style.stroke = blue;
+        }
         back.style.background = "white";
-        left_line.style.background = "#ADCAD3";
-        right_line.style.background = "#D56969";
-        for (var i = 0; i < nav_items.length; i++) {
-        	nav_items[i].style.color = "#707070";
-        }
+        left_line.style.background = blue;
+        right_line.style.background = red;
+        the_overlay_color.style.zIndex = 1;
+        the_overlay.style.zIndex = 3;
     }else{
-        back.style.background = "#ADCAD3";
+        
         for (var i = 0; i < nav_items.length; i++) {
-        	nav_items[i].style.color = "#707070";
+        	nav_items[i].style.color = grey;
         }
-        left_line.style.background = "#707070";
-        right_line.style.background = "#D56969";
+        for (var i = 0; i < stroke1.length; i++) {
+        	stroke1[i].style.stroke = grey;
+        }
+        back.style.background = blue;
+        left_line.style.background = grey;
+        right_line.style.background = red;
+        the_overlay_color.style.zIndex = 1;
+        the_overlay.style.zIndex = 3;  
     }
   });
 	$("#grey-circle").click(function() {
@@ -48,23 +82,48 @@
     var right_line = document.getElementById("red-line");
     var left_line = document.getElementById("blue-line");
     var nav_items = document.getElementsByTagName("a");
+    var stroke1 = document.getElementsByClassName("text text-stroke text-stroke-2");
+    var the_overlay_color = document.getElementById("overlay-color");
+    var the_overlay = document.getElementById("overlay");
 
 	if(back.style.background === "rgb(112, 112, 112)"){
+        for (var i = 0; i < nav_items.length; i++) {
+        	nav_items[i].style.color = grey;
+        }
+        for (var i = 0; i < stroke1.length; i++) {
+        	stroke1[i].style.stroke = blue;
+        }
         back.style.background = "white";
-        left_line.style.background = "#ADCAD3";
-        right_line.style.background = "#D56969";
-        for (var i = 0; i < nav_items.length; i++) {
-        	nav_items[i].style.color = "#707070";
-        }
+        left_line.style.background = blue;
+        right_line.style.background = red;
+        the_overlay_color.style.zIndex = 1;
+        the_overlay.style.zIndex = 3;
     }else{
-        back.style.background = "#707070";
         for (var i = 0; i < nav_items.length; i++) {
-        	nav_items[i].style.color = "#ADCAD3";
+        	nav_items[i].style.color = red;
         }
-        left_line.style.background = "#ADCAD3";
-        right_line.style.background = "#D56969";
+        for (var i = 0; i < stroke1.length; i++) {
+        	stroke1[i].style.stroke = blue;
+        }
     }
+    back.style.background = grey;
+    left_line.style.background = blue;
+    right_line.style.background = red;
+    the_overlay_color.style.zIndex =3;
+    the_overlay.style.zIndex = 1;
     });
+
+$(function() {
+  $('.intro').addClass('go');
+
+  // $('.reload').click(function() {
+  //   $('.intro').removeClass('go').delay(200).queue(function(next) {
+  //     $('.intro').addClass('go');
+  //     next();
+  //   });
+
+  // });
+})
   
 	
 
